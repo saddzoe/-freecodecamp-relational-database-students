e7f0d8df1d5149b7167741524b09eae0f8a7bd40
Mohammed Olaiya <mohammedolaiya@gmail.com>
2022-11-20 01:04:28 -0500

    Update README.sql

--- README.sql
+++ README.sql
@@ -30,3 +30,27 @@ CREATE DATABASE students WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE 
 ALTER DATABASE students OWNER TO freecodecamp;
 
 \connect students
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: courses; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.courses (
+    course_id integer NOT NULL,
+    course character varying(100) NOT NULL
+);
